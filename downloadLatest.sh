c33c48055b5c57125023c3c33370aa34e2e3e3fe
srinandan <13950006+srinandan@users.noreply.github.com>
2022-10-24 10:59:14 -0700

    fix filename

--- downloadLatest.sh
+++ downloadLatest.sh
@@ -85,7 +85,7 @@ printf "\n"
 # setup INTEGRATIONCLI
 cd "$HOME" || exit
 mkdir -p "$HOME/.integrationcli/bin"
-mv "${tmp}/INTEGRATIONCLI_${INTEGRATIONCLI_VERSION}_${OSEXT}_${INTEGRATIONCLI_ARCH}/integrationcli" "$HOME/.integrationcli/bin"
+mv "${tmp}/integrationcli_${INTEGRATIONCLI_VERSION}_${OSEXT}_${INTEGRATIONCLI_ARCH}/integrationcli" "$HOME/.integrationcli/bin"
 printf "Copied integrationcli into the $HOME/.integrationcli/bin folder.\n"
 chmod +x "$HOME/.integrationcli/bin/integrationcli"
 rm -r "${tmp}"
