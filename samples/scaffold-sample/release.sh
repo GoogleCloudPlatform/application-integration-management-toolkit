c67a520575f0545588a4fa75b2bea3cdaea8f64b
srinandan <srinandans@google.com>
2025-01-06 03:21:48 +0000

    adds target flag to release

--- samples/scaffold-sample/release.sh
+++ samples/scaffold-sample/release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gcloud deploy releases create test-release-$((RANDOM % 900 + 100)) --project=${PROJECT_ID} --region=${LOCATION} --delivery-pipeline=appint-sample-pipeline
+gcloud deploy releases create test-release-$((RANDOM % 900 + 100)) --project=${PROJECT_ID} --region=${LOCATION} --to-target=dev-env --delivery-pipeline=appint-sample-pipeline
