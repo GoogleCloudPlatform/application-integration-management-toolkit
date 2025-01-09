324774fa5bc86030e4a5e4f54a0fefe897e91df2
srinandan <srinandans@google.com>
2025-01-09 00:20:19 +0000

    feat: addds basic info as a preference #330

--- internal/cmd/preferences/setpref.go
+++ internal/cmd/preferences/setpref.go
@@ -58,11 +58,17 @@ var SetCmd = &cobra.Command{
 			}
 		}
 
+		if basic {
+			if err = apiclient.SetBasic(basic); err != nil {
+				return err
+			}
+		}
+
 		return nil
 	},
 }
 
-var nocheck bool
+var nocheck, basic bool
 
 func init() {
 	var project, region, proxyURL string
@@ -82,4 +88,7 @@ func init() {
 
 	SetCmd.Flags().Var(&api, "api", "Sets the control plane API. Must be one of prod, "+
 		"staging or autopush; default is prod")
+
+	SetCmd.Flags().BoolVarP(&basic, "basic", "",
+		false, "For supported commands, return basic information")
 }
